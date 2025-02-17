b3f44ec071704e2c8f4149fb8b3611ec2e2c5eb5
21086 <21086@wc.school.nz>
2025-02-18 12:22:22 +1300

    Added Contact and menu routes.

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def render_home():  # put application's code here
+def render_home():
     return render_template('home.html')
 
+
+@app.route('/menu')
+def render_menu():
+    return render_template('menu.html')
+
+
+@app.route('/contact')
+def render_contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
