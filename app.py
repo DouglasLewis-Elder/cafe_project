2e109e1c4303f88b0d6cf89f164a42e2461d77a4
21086 <21086@wc.school.nz>
2025-02-18 12:19:12 +1300

    Added Templates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_home():  # put application's code here
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
